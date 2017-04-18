e675bdc1eee7f055ba07e580c5f537f1b21f0f89
Ceren Avci <s.cerenavci@gmail.com>
2017-04-19 00:16:50 +0300

    Created NeedleMovementScript

---
+++ Assets/Scripts/NeedleMovementScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeedleMovementScript : MonoBehaviour {
+
+	// Use this for initialization
+	[SerializeField]
+    private GameObject needleBody;
+
+    private bool canFireNeedle; //initially equal to false
+    private bool touchedTheCircle;
+    private float forceY = 70f;
+    private Rigidbody2D myBody;
+
+    void Awake()
+    {
+        Initialize();
+
+    }
+
+    void Initialize()
+    {
+        needleBody.SetActive(false);
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (canFireNeedle)
+            myBody.velocity = new Vector2(0, forceY);
+
+    }
+
+    public void FireTheNeedle()
+    {
+        needleBody.SetActive(true);
+        myBody.isKinematic = false; //it it' true => physics is not affecting it
+        canFireNeedle = true;
+    }
+}
