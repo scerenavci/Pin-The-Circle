14f0c96816e60733e6799d3feaed38a4836216a7
Ceren Avci <s.cerenavci@gmail.com>
2017-05-02 22:16:19 +0300

    Created Main Menu

---
+++ Assets/Scripts/Main Menu Controller/MainMenuController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+    public void PlayGame()
+    {
+        Application.LoadLevel("GamePlay");
+    }
+}
